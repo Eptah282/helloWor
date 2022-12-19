348b935177179e3df863238b448cf93806fcb5a6
Vlad'ka <vlad.gorodkov@mail.ru>
2022-12-19 14:48:53 +0400

    Initial Commit

---
+++ helloWor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  helloWor
+//
+//  Created by eptah on 19.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
