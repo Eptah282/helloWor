5c48cd1615f7f58548a6edccc2dbf64fdd5cf979
Vlad'ka <vlad.gorodkov@mail.ru>
2022-12-28 17:31:08 +0400

    add 3 button

--- helloWor/ViewController.swift
+++ helloWor/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var helloWorld: UILabel!
+    
+    @IBOutlet var toggleButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        helloWorld.isHidden = true
+        toggleButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func buttonPressed() {
+        if  helloWorld.isHidden {
+            helloWorld.isHidden = false
+            toggleButton.setTitle("Hide", for: .normal)
+        }else {
+            helloWorld.isHidden = true
+            toggleButton.setTitle("Show Texe", for: .normal)
+        }
+    }
+    
+    
+    
 }
-
